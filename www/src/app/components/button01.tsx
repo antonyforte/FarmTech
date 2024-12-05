144838bdb1c919669e0406755e298d6775275904
LuisAssis2002 <105814989+LuisAssis2002@users.noreply.github.com>
2024-12-04 22:58:20 -0300

    Delete www directory

--- www/src/app/components/button01.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button01(props : any) {
-    return (
-        <button onClick={props.handler} className="flex justify-center items-center h-[62px] w-[153px] text-3xl text-white bg-orange-500 border-[1px] border-slate-900">
-            <h1>{props.text}</h1>
-        </button>
-    )
-}
