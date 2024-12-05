74e77bcddd6bb819a22f463eca1723ba1db0d1bd
antonyforte <antoniomontgue@gmail.com>
2024-12-04 22:27:43 -0300

    update server2 on master

--- server2/src/routes/usersAccount.ts
+++ server2/src/routes/usersAccount.ts
@@ -1,57 +1,52 @@
 import type { FastifyInstance } from "fastify";
-import { prisma } from '../lib/prisma'
-import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
 
 export async function usersAccountsRoutes(app: FastifyInstance) {
-    
-    app.put('/users/:cpf', async (request, reply) => {
-        
-        const paramsSchema = z.object({
-            cpf: z.string(),
-        });
-        const bodySchema = z.object({
-            name: z.string().optional(),
-            email: z.string().email().optional(),
-            password: z.string().optional(),
-        });
-        const { cpf } = paramsSchema.parse(request.user?.usercpf);
+    app.put("/users/:cpf", async (request, reply) => {
+        const bodySchema = z
+            .object({
+                name: z.string().optional(),
+                email: z.string().email().optional(),
+                password: z.string().optional(),
+            })
+            .refine((data) => Object.keys(data).length > 0, {
+                message: "Pelo menos um campo deve ser enviado para atualização.",
+            });
 
-        if(!cpf){
-            return reply.status(401).send({ message: 'Usuário não autenticado'});
+        const usercpf = request.user?.usercpf;
+
+        if (!usercpf) {
+            return reply.status(401).send({ message: "Usuário não autenticado" });
         }
 
         const { name, email, password } = bodySchema.parse(request.body);
 
         try {
             const updatedUser = await prisma.user.update({
-                where: { cpf },
-                data: { name, email, password},
+                where: { cpf: usercpf },
+                data: { name, email, password },
             });
             return reply.status(200).send(updatedUser);
         } catch (error) {
-            return reply.status(400).send({ message: "Erro ao atualizar Usuário."});
+            return reply.status(400).send({ message: "Erro ao atualizar Usuário." });
         }
-    })
+    });
 
-    app.delete('/users/:cpf', async (request, reply) => {
-        
-        const paramsSchema = z.object({
-            cpf: z.string(),
-        });
-        const { cpf } = paramsSchema.parse(request.user?.usercpf);
+    app.delete("/users/:cpf", async (request, reply) => {
+        const usercpf = request.user?.usercpf;
 
-        if(!cpf){
-            return reply.status(401).send({ message: 'Usuário não autenticado'});
+        if (!usercpf) {
+            return reply.status(401).send({ message: "Usuário não autenticado" });
         }
 
         try {
             await prisma.user.delete({
-                where: {cpf},
+                where: { cpf: usercpf },
             });
-            return reply.status(200).send({message: "Usuário deletado com sucesso"})
+            return reply.status(200).send({ message: "Usuário deletado com sucesso" });
         } catch (error) {
-            reply.status(400).send({message: "Erro ao deletar usuário"})
+            reply.status(400).send({ message: "Erro ao deletar usuário" });
         }
-    })
-
+    });
 }
